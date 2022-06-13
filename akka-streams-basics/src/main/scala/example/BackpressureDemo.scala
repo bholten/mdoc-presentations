ce3c9da660a061b17f574604b0964c83817a46be
Brennan Holten <brennan.holten@47deg.com>
2022-06-13 10:16:14 -0500

    Operators

--- akka-streams-basics/src/main/scala/example/BackpressureDemo.scala
+++ akka-streams-basics/src/main/scala/example/BackpressureDemo.scala
@@ -35,24 +35,6 @@ object BackpressureDemo {
     val throttledSource = fastSource.throttle(1, 2.second)
     throttledSource.via(bufferedFlow).to(slowSink).run()
 
-    val source = Source(1 to 100)
-
-    source.recover { case _: RuntimeException =>
-      42
-    }
-
-    source recoverWithRetries (3, { case _: RuntimeException =>
-      Source(101 to 200)
-    })
-
-    RestartSource.onFailuresWithBackoff(
-      RestartSettings(
-        minBackoff = 1.second,
-        maxBackoff = 2.seconds,
-        randomFactor = 0.1
-      )
-    )(() => source)
-
     ()
   }
 }
